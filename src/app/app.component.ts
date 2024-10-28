0b8b0ba6b451a879f1e3cdddf7a99ef1176d15c7
Ovie Revaldi <ovierevaldi@gmail.com>
2024-10-28 10:53:51 +0700

    Add tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewsSmallComponent } from "./components/news-small/news-small.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewsSmallComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/news-small/news-small.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-small',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-small.component.html',
+  styleUrl: './news-small.component.css'
+})
+export class NewsSmallComponent {
+
+}
