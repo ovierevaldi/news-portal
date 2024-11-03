6b38cadb3ef05e9cbccaacfe8f99cf2dd4f94761
Ovie Revaldi <ovierevaldi@gmail.com>
2024-11-03 18:41:22 +0700

    update .env variable

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    api_keys: process.env['api_keys'] || '',
+    api_secret: process.env['api_secret'] || '',
+    API_URL: process.env['API_URL'] || ''
+}
